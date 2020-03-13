7fb79873a8d45fae797d89fb721c7cc880374885
Javid <subhan.javid@xelits.com>
2020-03-13 22:30:53 +0530

    admin product edit tab

---
+++ AdminProductEditTab/Block/Adminhtml/Product/Edit/Tab/CustomData.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Javid\AdminProductEditTab\Block\Adminhtml\Product\Edit\Tab;
+use Magento\Backend\Block\Template\Context;
+use Magento\Framework\Registry;
+
+class CustomData extends \Magento\Framework\View\Element\Template
+{
+    protected $_template = 'customdata.phtml';
+
+    protected $_coreRegistry = null;
+
+    public function __construct(
+        Context $context,
+        Registry $registry,
+        array $data = []
+    )
+    {
+        $this->_coreRegistry = $registry;
+        parent::__construct($context, $data);
+    }
+
+    public function getProduct()
+    {
+        return $this->_coreRegistry->registry('current_product');
+    }
+}
